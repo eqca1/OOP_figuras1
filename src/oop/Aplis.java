3171d5bf9f3d9810819b9f38d6ca4e53b9186dab
qwe <h@gmail.com>
2025-09-23 12:04:48 +0300

    Pabeigta klase Trijsturis

--- src/oop/Aplis.java
+++ src/oop/Aplis.java
@@ -1,5 +1,25 @@
 package oop;
 
 public class Aplis {
-
+	public Centrs punkts;
+	public double radiuss;
+	
+	public Aplis (Centrs p, double r) {
+		punkts = p;
+		radiuss = r;
+	}
+	
+	public String izvaditAplaInfo() {
+		return "Rādiuss - "+radiuss+" cm.";
+	}
+	
+	public double rLinija() {
+		double C = Math.PI*2*radiuss;
+		return Math.round(C*100.0)/100.0;
+	}
+	public double rLaukums() {
+		double L = Math.PI*Math.pow(radiuss, 2);
+		return Math.round(L*100.0)/100.0;
+		
+	}
 }
--- src/oop/Trijsturis.java
+++ src/oop/Trijsturis.java
@@ -2,4 +2,24 @@ package oop;
 
 public class Trijsturis {
 
+	public double malaA, malaB, malaC;
+	public Centrs punkts;
+	
+	public Trijsturis(Centrs p, double m1, double m2, double m3) {
+		punkts = p;
+		malaA = m1;
+		malaB = m2;
+		malaC = m3;
+	}
+	
+	public String izvaditTrijsturaInfo() {
+		return "Mala A - "+malaA+" cm.\nMala B - "+malaB+" cm.\nMala C - "+malaC+" cm.";
+	}
+	public double tLaukums() {
+		
+		double p = (malaA+malaB+malaC)/2;
+		double L = Math.sqrt(p*(p-malaA)*(p-malaB)*(p-malaC));
+		return Math.round(L*100.0)/100.0;
+		
+	}
 }
