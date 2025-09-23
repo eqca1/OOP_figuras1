b77fbedbfd92144c4bd2bd4ce91a132ca6d9be7e
qwe <h@gmail.com>
2025-09-23 11:35:40 +0300

    Pabeigta klase Cetrsturis

--- src/oop/Cetrsturis.java
+++ src/oop/Cetrsturis.java
@@ -1,6 +1,27 @@
 package oop;
 
 public class Cetrsturis {
-		
+	public double platums, augstums;
+	public Centrs punkts;
 	
-}
+	public Cetrsturis() {
+		punkts = new Centrs(0, 0);
+	}
+	public Cetrsturis(Centrs punkts, double p, double a) {
+	
+		this.punkts = punkts;
+		platums = p;
+		augstums = a;
+			
+			
+		}
+	public String izvaditCetrsturaInfo() {
+		return "Platums - "+platums+" cm./nAugstums - "+augstums+" cm.";
+	}
+	public double cLaukums() {
+		double L;
+		L = platums * augstums;
+		return L;
+	}
+	}
+	
